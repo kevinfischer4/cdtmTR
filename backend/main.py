2161144119b1b337ae6b2218fcacfda06a01f089
Alexander Feix <alexander.feix03@gmail.com>
2025-05-10 16:02:37 +0200

    added total return

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model import Person
-from backend_handler import get_overall_user_data, get_friends
+from backend_handler import get_overall_user_data, get_friends_data, get_portfolio_data
 from database_handler import connect_to_database
 
 app = FastAPI()
@@ -15,7 +15,7 @@ async def get_user_overall(user_id: str):
     
 @app.get("/friends/")
 async def get_friends_data(user_id: str):
-    friends_objects = get_friends(user_id)
+    friends_objects = get_friends_data(user_id)
     friends = []
     for friend in friends_objects:
         friends.append({
@@ -25,20 +25,15 @@ async def get_friends_data(user_id: str):
             "latest": "",
             "avatarLink": friend.avatar_link
         })
-    friends.append({"friendUser1": {
-        "firstName": "Alexander", 
-        "lastName": "Feix",
-        "traderProfile": "ai summary from trader profile",
-        "latest": "latest ai summary",
-        "avatarLink": "avatar/lnik"
-        }})
     return friends
     
     
 @app.get("/user/details/")
 async def get_user_details(user_id: str, category: str):
-    return {"summary": "test-summary",
-            "riskRatio": 3.0,
-            "riskText": "risk is risky",
-            "totalReturn": 100.0,
+    portfolio = get_portfolio_data(cur, user_id)
+    
+    return {"summary": portfolio.summary,
+            "riskRatio": portfolio.risk_ratio,
+            "riskText": portfolio.risk_summary,
+            "totalReturn": portfolio.to,
             "assets": {"TEST_ASSET_NAME": 67.0, "TEST_ASSET_2": 23.0}}
